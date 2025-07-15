6cdcb7fb509f2198d41cd8785a2fb40ecc09de02
Barae Matrane <baraamatrane@gmail.com>
2025-07-15 08:11:42 +0100

    message

---
+++ lib/models/User.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  Type: { type: String, enum: ["user", "event"], default: "user" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
