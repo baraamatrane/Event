f3801624a74f07e602c69ce50a56554a815f233b
Barae Matrane <baraamatrane@gmail.com>
2025-07-28 12:25:37 +0100

    update route

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import Category from "../../../../../lib/models/Category";
-import connectDB from "../../../../../lib/db";
+import Category from "../../../../lib/models/Category";
+import connectDB from "../../../../lib/db";
 
 export async function POST(req: NextRequest) {
   try {
