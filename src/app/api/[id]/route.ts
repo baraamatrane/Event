a9ac42c27e3ba4e8024a27cc40eee00bbb2ea8d7
Barae Matrane <baraamatrane@gmail.com>
2025-08-13 16:00:50 +0100

    update

--- src/app/api/[id]/route.ts
+++ src/app/api/[id]/route.ts
@@ -1,23 +1,6 @@
 import mongoose from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-  const { pathname } = new URL(request.url);
-  const id = pathname.split("/").pop();
-  if (
-    !pathname.includes("/users/") ||
-    !id ||
-    !mongoose.Types.ObjectId.isValid(id)
-  ) {
-    return new Response("Invalid path", { status: 400 });
-  }
-  const Finduser = await mongoose.models.User.findById(id);
-  if (!Finduser) {
-    return new Response("User not found", { status: 404 });
-  }
-  return NextResponse.json(Finduser, { status: 200 });
-}
-
 export async function PUT(request: NextRequest) {
   try {
     const data = await request.json();
